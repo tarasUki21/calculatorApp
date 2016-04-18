3b3d73db310003306879a5ccf9d9fca8a2f902dd
tarasUki21 <taras.z31@gmail.com>
2016-04-18 17:02:41 -0400

    shrinking of text added, need to adjust numbers for good ratio

--- Calculator/app/src/main/java/com/example/taras/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/taras/calculator/MainActivity.java
@@ -2,30 +2,37 @@ package com.example.taras.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    TextView etValue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        etValue = (TextView) findViewById(R.id.textView);
     }
-    TextView etValue = (TextView) findViewById(R.id.textView);
+
     double operand = 0;
     double operator = 0;
     String operation = "";
 
 
     public void firstInput(){
-        if (etValue.getText().equals("input required")){
+        if (etValue.getText().equals("input required")){ // first input
             etValue.setText("");
         }
+        if(etValue.length() % 9 == 0){//if the box fills up adjust size of text
+            float sizeOfText = (etValue.getTextSize() - 5.0f) / 3;
+            etValue.setTextSize(sizeOfText);
+            //etValue.setText(Float.toString(sizeOfText - 20.0f));
+        }
     }
-    public void populate(){
+    public void populate(){ //populates the two variables for later calculations
         if(operation.length() > 0){
             operator = Double.parseDouble(etValue.getText().toString());
         }
@@ -33,62 +40,62 @@ public class MainActivity extends AppCompatActivity {
             operand = Double.parseDouble(etValue.getText().toString());
         }
     }
-    public void oneButtonPress(){
+    public void oneButtonPress(View v){//seperate method for each button for easy changes
         firstInput();
         etValue.append("1");
         populate();
 
     }
 
-    public void twoButtonPress(){
+    public void twoButtonPress(View v){
         firstInput();
         etValue.append("2");
         populate();
 
     }
-    public void threeButtonPress(){
+    public void threeButtonPress(View v){
         firstInput();
         etValue.append("3");
         populate();
 
     }
-    public void fourButtonPress(){
+    public void fourButtonPress(View v){
         firstInput();
         etValue.append("4");
         populate();
 
     }
-    public void fiveButtonPress(){
+    public void fiveButtonPress(View v){
         firstInput();
         etValue.append("5");
         populate();
 
     }
-    public void sixButtonPress(){
+    public void sixButtonPress(View v){
         firstInput();
         etValue.append("6");
         populate();
 
     }
-    public void sevenButtonPress(){
+    public void sevenButtonPress(View v){
         firstInput();
         etValue.append("7");
         populate();
 
     }
-    public void eightButtonPress(){
+    public void eightButtonPress(View v){
         firstInput();
         etValue.append("8");
         populate();
 
     }
-    public void nineButtonPress(){
+    public void nineButtonPress(View v){
         firstInput();
         etValue.append("9");
         populate();
 
     }
-    public void zeroButtonPress(){
+    public void zeroButtonPress(View v){
         firstInput();
         etValue.append("0");
         populate();
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# calculatorApp
-One two three
+# calculatorApp 
+by Taras Zelenenkyy
+hey, I created this app as a project to pass time. All of this is created using
+Android Studio 1.5.1. 
+I am not making profit from this.  I want to create a free easy app to use for graphing and or calculations with no hassle. 
+
+This is a work in progress and not even close to being done.
+
+I do not allow anyone to use my code for their own profit but encourage
+everyone to learn thanks.  :)
