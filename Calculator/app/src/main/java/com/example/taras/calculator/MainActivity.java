0fb2ff023caa527b8a0e7f827f5a0385adc7008f
tarasUki21 <taras.z31@gmail.com>
2016-04-18 21:02:00 -0400

    added operations, first calculation passes, need to clear screen for second one, otherwise crash.

--- Calculator/app/src/main/java/com/example/taras/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/taras/calculator/MainActivity.java
@@ -4,101 +4,73 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView etValue;
+    TextView etValue, answerVal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         etValue = (TextView) findViewById(R.id.textView);
+        answerVal = (TextView) findViewById(R.id.textView2);
     }
 
-    double operand = 0;
-    double operator = 0;
+    double operandA = 0;
+    double operandB = 0;
     String operation = "";
 
 
-    public void firstInput(){
-        if (etValue.getText().equals("input required")){ // first input
+    public void firstInput() {
+        if (etValue.getText().equals("input required")) { // first input
             etValue.setText("");
         }
-        if(etValue.length() % 9 == 0){//if the box fills up adjust size of text
-            float sizeOfText = (etValue.getTextSize() - 5.0f) / 3;
-            etValue.setTextSize(sizeOfText);
-            //etValue.setText(Float.toString(sizeOfText - 20.0f));
+
+        if (etValue.length() % 6 == 0) {//if the box fills up adjust size of text
+            if (etValue.getTextSize() > 100) {
+                float sizeOfText = (etValue.getTextSize() - 20.0f) / 3;
+
+                etValue.setTextSize(sizeOfText);
+            }
+        }
+
+    }
+
+    public void populate() { //populates the two variables for later calculations
+        if (operation.length() > 0) {
+            operandA = Double.parseDouble(etValue.getText().toString());
+        } else {
+            operandB = Double.parseDouble(etValue.getText().toString());
         }
     }
-    public void populate(){ //populates the two variables for later calculations
-        if(operation.length() > 0){
-            operator = Double.parseDouble(etValue.getText().toString());
+
+    public void genButtonPress(View v) {//number button
+        firstInput();
+        etValue.append(((Button) v).getText());
+        populate();
+
+    }
+
+    public void opButtonPress(View v) {//operation button
+        operation = ((Button) v).getText().toString();
+        etValue.setText("");
+
+    }
+
+    public void equButtonPress(View v) {
+        if (operation.equals("+")) {
+            answerVal.setText(Double.toString(operandA + operandB));
+        } else if (operation.equals("-")) {
+            answerVal.setText(Double.toString(operandA - operandB));
+        } else if (operation.equals("*")) {
+            answerVal.setText(Double.toString(operandA * operandB));
+        } else if (operation.equals("/")) {
+            answerVal.setText(Double.toString(operandA / operandB));
         }
-        else{
-            operand = Double.parseDouble(etValue.getText().toString());
-        }
-    }
-    public void oneButtonPress(View v){//seperate method for each button for easy changes
-        firstInput();
-        etValue.append("1");
-        populate();
-
-    }
-
-    public void twoButtonPress(View v){
-        firstInput();
-        etValue.append("2");
-        populate();
-
-    }
-    public void threeButtonPress(View v){
-        firstInput();
-        etValue.append("3");
-        populate();
-
-    }
-    public void fourButtonPress(View v){
-        firstInput();
-        etValue.append("4");
-        populate();
-
-    }
-    public void fiveButtonPress(View v){
-        firstInput();
-        etValue.append("5");
-        populate();
-
-    }
-    public void sixButtonPress(View v){
-        firstInput();
-        etValue.append("6");
-        populate();
-
-    }
-    public void sevenButtonPress(View v){
-        firstInput();
-        etValue.append("7");
-        populate();
-
-    }
-    public void eightButtonPress(View v){
-        firstInput();
-        etValue.append("8");
-        populate();
-
-    }
-    public void nineButtonPress(View v){
-        firstInput();
-        etValue.append("9");
-        populate();
-
-    }
-    public void zeroButtonPress(View v){
-        firstInput();
-        etValue.append("0");
-        populate();
-
+        etValue.setText("Input Required");
     }
 }
